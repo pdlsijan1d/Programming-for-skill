142f27b3f9ffbacf8f73c1b2cf8c7ec09d8fccf1
pdlsijan1d <pdlsijan1@gmail.com>
2026-06-04 18:02:40 +0545

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i, num, fact=1;
+    printf("Enter a number:");
+    scanf("%d",&num);
+
+    for(i=1;i<=num;i++)
+    {
+        fact = fact * i;
+    }
+
+    printf("The factorial of %d is %d",num,fact);
+return 0;
+}
